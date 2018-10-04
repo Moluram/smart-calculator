b74324c30024a3a84d98562a9e6c1de5ca1cfd48
achorny <moluram.a@gmail.com>
2018-10-04 15:08:49 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.value = '';
+        this.lastValue = initialValue + '';
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+        SmartCalculator.prototype.toString = () => eval(this.value + this.lastValue);
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.value += this.lastValue + "+";
+        this.lastValue = number + '';
+        return this
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.value += this.lastValue + "-";
+        this.lastValue = number + '';
+        return this
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.value += this.lastValue + "*";
+        this.lastValue = number + '';
+        return this
+    }
+
+    devide(number) {
+        this.value += this.lastValue + "/";
+        this.lastValue = number + '';
+        return this
+    }
+
+    pow(number) {
+        if (this.lastValue.includes('Math.pow(')) {
+            let end = this.lastValue.split(',').pop();
+            let start = this.lastValue.replace(end, '');
+            let power = end.split(')')[0];
+            this.lastValue = start + "Math.pow(" +  power + "," + number + ")" + end.replace(power, '');
+        } else {
+            this.lastValue = "Math.pow(" + this.lastValue + "," + number + ")";
+        }
+        return this
+    }
 }
 
 module.exports = SmartCalculator;
